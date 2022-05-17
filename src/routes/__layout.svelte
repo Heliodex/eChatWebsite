ccd60e6b2abeaf2e390d823e0d7ea99430f6860d
Lewin Kelly <heli@odex.cf>
2022-05-17 17:06:02 +0100

    Add header and eChat icon

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,12 @@
 </script>
 
 <header>
-	<h1>eChat</h1>
+	<div class="left">
+		<img class="headerlogo" src="white.svg" alt="eChat logo"/>
+	</div>
+	<div class="right">
+		test
+	</div>
 </header>
 
 <main>
