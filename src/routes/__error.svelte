91422a0e62bd481f6960bae4aeaec4cf4c4b4961
Lewin Kelly <heli@odex.cf>
2022-05-26 01:15:20 +0100

    Add error page, displays error message

---
+++ src/routes/__error.svelte
@@ -0,0 +1,22 @@
+<script lang="ts" context="module">
+	export function load({ error, status }: { error: object; status: number }) {
+		return {
+			props: {
+				status: status,
+				error: error,
+			},
+		}
+	}
+	// copied straight from kit.svelte.dev
+</script>
+
+<script lang="ts">
+	export let status: number
+	export let error: any
+</script>
+
+<br />
+<h1>
+	Error {status}
+</h1>
+{error["message"]}
