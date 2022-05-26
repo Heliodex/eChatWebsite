b07ae07ba569ec36ac7c10556be2a67a97c45f98
Donald Wright <96298159+d0naldw@users.noreply.github.com>
2022-05-26 17:14:54 +0100

    Sass cleaning up

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,11 +6,11 @@
 	<div>
 		<img class="eccb" src="eccb.png" alt="eChat on a Chromebook 11 3189" />
 	</div>
-	<div>
+	<div class="logoandtext">
 		<img class="mainLogo" src="green.svg" alt="Green eChat logo" />
 		<p>
 			A simple chat extension.<br />
-			Chat in public groups, or one on one.<br />
+			Chat in groups, or one on one.<br />
 		</p>
 		<button class="cwsButton">
 
