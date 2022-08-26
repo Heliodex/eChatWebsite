58e357311c946d0774ec96b48a788165e6e55a4e
Lewin Kelly <heli@odex.cf>
2022-08-27 00:08:47 +0100

    Fully migrate to latest SvelteKit

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,28 +1,17 @@
-<script lang="ts" context="module">
-	throw new Error("@migration task: Replace error load function (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3293209)");
-
-	// export function load({ error, status }: { error: object; status: number }) {
-	// 	return {
-	// 		props: {
-	// 			status: status,
-	// 			error: error,
-	// 		},
-	// 	}
-	// }
-	// // copied straight from kit.svelte.dev
-</script>
-
 <script lang="ts">
-	export let status: number
-	export let error: any
+	import { page } from "$app/stores"
 </script>
+
+<svelte:head>
+	<title>eChat - Error {$page.status}</title>
+</svelte:head>
 
 <div class="thinpage">
 	<div class="thintext">
 		<br />
 		<h1>
-			Error {status}
+			Error {$page.status}
 		</h1>
-		{error["message"]}
+		{$page.error.message}
 	</div>
 </div>
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { sveltekit } from "@sveltejs/kit/vite"
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+	},
+}
+
+export default config
