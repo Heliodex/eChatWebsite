f8d91eef9e6c880409e4c8115173b41c7abeaa58
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-08-26 23:52:14 +0100

    Migrate to latest SvelteKit: Rename files

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<script lang="ts" context="module">
+	throw new Error("@migration task: Replace error load function (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3293209)");
+
+	// export function load({ error, status }: { error: object; status: number }) {
+	// 	return {
+	// 		props: {
+	// 			status: status,
+	// 			error: error,
+	// 		},
+	// 	}
+	// }
+	// // copied straight from kit.svelte.dev
+</script>
+
+<script lang="ts">
+	export let status: number
+	export let error: any
+</script>
+
+<div class="thinpage">
+	<div class="thintext">
+		<br />
+		<h1>
+			Error {status}
+		</h1>
+		{error["message"]}
+	</div>
+</div>
