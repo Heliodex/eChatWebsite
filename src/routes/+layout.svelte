79508e29188e9cadb0ee7a9a3aecd24dcea7f4e6
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-08-28 05:21:58 +0100

    Move sass from global into pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,3 +18,72 @@
 <main>
 	<slot />
 </main>
+
+<style lang="sass">
+	.headerlogo
+		visibility: hidden
+
+	header
+		width: 100%
+		font-size: 1.2rem
+		top: 0
+		height: 65px
+		position: fixed
+		align-content: center
+		justify-content: center
+		display: flex
+
+		background-color: #31a47e
+
+		.headerContainer
+			width: 100%
+			max-width: 1200px
+
+			display: flex
+			justify-content: space-between
+			align-items: center
+			align-content: center
+
+			.right
+				margin-right: 7rem
+				padding: 0
+				display: flex
+				float: left
+				justify-content: flex-end
+				right: 1rem
+
+			.headerlogo
+				margin-left: min(8vw, 7rem)
+				img
+					height: 33px
+
+			a
+				min-width: 10%
+				margin: 0 1.5rem
+				color: white
+				text-decoration: none
+				transition: filter 0.2s
+				&:hover
+					transition: filter 0.2s
+					filter: opacity(80%)
+	
+	.right
+		text-align: right
+		padding: 0 7rem
+
+	@media screen and (max-width: 599px)
+
+		header
+			grid-template-columns: 0fr 1fr
+			justify-content: center
+
+		.right
+			width: 100%
+			a
+				min-width: 2rem
+
+	@media screen and (min-width: 600px)
+
+		.headerlogo
+			visibility: visible
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,9 +13,59 @@
 				A simple chat extension.<br />
 				Chat in groups, or one on one.<br />
 			</p>
-			<button class="cwsButton">
-				<img class="cwsLogo" src="webstore.png" alt="Get it on the Chrome Web Store" />
-			</button>
 		</div>
 	</div>
 </div>
+
+<style lang="sass">
+	.eccb
+		position: absolute
+		object-fit: contain
+		height: 80vh
+		margin-top: 3%
+		right: 50%
+		max-width: 80vh
+	
+	.grid
+		width: 100%
+		display: grid
+		grid-template-columns: 1fr 1fr
+		column-gap: 0.7rem
+
+	.eccb
+		visibility: hidden
+
+	.homepageInfo
+		margin-left: 2rem
+		display: flex
+		flex-direction: column
+
+	.mainLogo
+		width: 30vw
+		max-width: 20rem
+		margin-top: 30vh
+
+	@media screen and (max-width: 599px)
+
+		.grid
+			grid-template-columns: 0fr 1fr
+			justify-content: center
+
+		.mainLogo
+			width: 60vw
+			max-width: 28rem
+
+		.homepageInfo
+			margin: 0
+		.homepageInfoContainer
+			display: flex
+			justify-content: center
+
+	@media screen and (min-width: 600px)
+
+		.eccb
+			visibility: visible
+
+		.headerlogo
+			margin-left: 3rem
+</style>
