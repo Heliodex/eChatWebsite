f89fe8b95c5091198f6da60d2477feb0308fbbb8
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-08-29 01:51:09 +0100

    Add installation instructions to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,30 +10,74 @@
 		<div class="homepageInfo">
 			<img class="mainLogo" src="green.svg" alt="Green eChat logo" />
 			<p>
-				A simple chat extension.<br />
+				A simple chat app in a popup.<br />
 				Chat in groups, or one on one.<br />
 			</p>
 		</div>
 	</div>
 </div>
+<div class="thinpage">
+	<div class="thintext">
+		<h1>How to install</h1>
+		<p>eChat was originally planned to be a browser extension, but these are often blocked on enterprise or school devices. Instead, we went with a different approach that allows eChat to be displayed over the top of almost any webpage.</p>
+		<br />
+		<h4>Step 1</h4>
+		<p>Press Ctrl + Shift + B in your browser to make sure that the Bookmarks Bar is visible.</p>
+		<h4>Step 2</h4>
+		<p>Drag this into any free space in your Bookmarks Bar: <a href="javascript:if(document.getElementById('eChatWindow')){'{'}const e=document.getElementById('eChatWindow');Object.assign(e.style,{'{'}display:'block'==getComputedStyle(e).getPropertyValue('display')?'none':'block'})}else{'{'}let e=document.createElement('script');e.src='https://cdn.jsdelivr.net/gh/heliodex/echatpopup/m.js',document.body.appendChild(e)}" id="bookmarklet">eChat</a></p>
+		<h4>Step 3</h4>
+		<p>
+			Click on the newly created bookmark. An eChat popup window should appear over the current website!<br />
+			You can drag the popup around by the header, close it using the close button, and re-open it by clicking the bookmark again.<br /><br />
+			Click the "i" icon in the top-left corner for help with using eChat.
+		</p>
+		<br /><br />
+		<h4 id="issues">Common issues</h4>
+		<p>
+			<em>- The popup does not appear after clicking the bookmark!</em><br />
+			This is likely because of CORS policies set by the owner of the current website, preventing the popup from loading. There's nothing we can do about this, sorry :(. Try using eChat on a different site.<br />
+		</p>
+		<p>
+			<em>- The popup appears after clicking the bookmark, but the eChat window is blank!</em><br />
+			This is probably because the site is using an insecure connection to the server. eChat does not operate over insecure connections, as this could allow an attacker to intercept messages. Try using eChat on a different site that shows a padlock icon in the URL bar.<br />
+		</p>
+		<p>If your issue is not listed here or is still ongoing, please contact our support team at <a href="mailto:help.echat@gmail.com">help.echat@gmail.com</a> so we can resolve the problem.</p>
+	</div>
+</div>
 
 <style lang="sass">
+	#bookmarklet
+		color: white
+		text-decoration: none
+		padding: 1.5%
+		margin: 0 0.5rem
+		border-radius: 0.5rem
+		pointer-events: drag
+
+	.thinpage
+		margin-top: 15vh
+
+	h1, #issues
+		text-align: center
+
+	h4
+		font-size: 1.25rem
+		margin: 0 0 0 2rem
+
 	.eccb
 		position: absolute
 		object-fit: contain
-		height: 80vh
+		height: 60vh
 		margin-top: 3%
 		right: 50%
-		max-width: 80vh
+		max-width: 60vh
+		visibility: hidden
 	
 	.grid
 		width: 100%
 		display: grid
 		grid-template-columns: 1fr 1fr
 		column-gap: 0.7rem
-
-	.eccb
-		visibility: hidden
 
 	.homepageInfo
 		margin-left: 2rem
@@ -65,7 +109,4 @@
 
 		.eccb
 			visibility: visible
-
-		.headerlogo
-			margin-left: 3rem
 </style>
