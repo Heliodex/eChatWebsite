3208afcb5f62d7cfc9c0ed77b2480f5c2fa291f9
Lewin Kelly <KEllyLewin2016@Gmail.com>
2023-01-13 23:50:29 +0000

    Add notice

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,6 +4,13 @@
 
 <div class="thinpage">
 	<div class="thintext">
+		<h1>Notice</h1>
+		<br />
+		<p>eChat has been disabled on school networks both by blocking bookmarks from executing javascript: urls. The bookmark will no longer open on these chromebooks.</p>
+		<p>The original eChat server has also been switched off, so data can no longer be sent or received by the client.</p>
+		<p>However, if you wish to run your own eChat instance, the code is now open-source and public domain. See the source code (and some instructions) at <a href="https://github.com/Heliodex/eChat" rel="noopener noreferrer">Github.com/Heliodex/eChat</a>.</p>
+		<p>We thank you for all of your support during the short few months that eChat was usable.</p>
+		<br />
 		<h1>About</h1>
 		<br />
 		<p>From the makers of DocSocial, eChat is a simple chat app in a popup. This allows for it to be untrackable by browser extensions, and for it to not appear in your history.</p>
